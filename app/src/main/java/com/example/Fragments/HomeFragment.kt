b0cd4f84a4550ad54afb8903967dcd46e0c3cf40
SunnyOneSoTrue <68395517+SunnyOneSoTrue@users.noreply.github.com>
2021-02-10 14:22:36 +0400

    added a change button, added new code for decoding.

--- app/src/main/java/com/example/Fragments/HomeFragment.kt
+++ app/src/main/java/com/example/Fragments/HomeFragment.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
@@ -15,6 +16,8 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
     private lateinit var outPutView : EditText
     private lateinit var etinput: EditText
     private lateinit var btnclear: Button
+    private lateinit var changeButton: Button
+    private var state:String = "T2S"
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -24,73 +27,154 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
         outPutView = view.findViewById(R.id.etoutput)
         etinput = view.findViewById(R.id.etinput)
         btnclear = view.findViewById(R.id.btnclear)
+        changeButton = view.findViewById(R.id.changeButton)
 
         translateButton()
         clearButton()
-
+        changeButton()
     }
+
     private fun translateButton(){
         btndecode.setOnClickListener {
-            val output = textToMorse(etinput.text.toString())
+            val output:String
+            if(state=="T2S") {
+                output = textToSypher(etinput.text.toString())
+            }
+            else{
+                output = sypherToText((etinput.text.toString()))
+            }
             outPutView.setText(output).toString()
 
         }
     }
+
     private fun clearButton() {
         btnclear.setOnClickListener {
             etinput.setText(null)
             outPutView.setText(null)
-
         }
-
-    }
-    private fun textToMorse(s: String): String {
-        val morseArr = s.map { value -> convertCharToMorse(value) }
-        (morseArr.joinToString(" "))
-
-
-        return (morseArr.joinToString(" "))
     }
 
+    private fun changeButton(){
+        changeButton.setOnClickListener{
+            if(state=="T2S") {
+                state = "S2T"
+                changeButton.setText("Sypher To Text")
+            }
+            else{
+                state = "T2S"
+                changeButton.setText("Text To Sypher")
+            }
+        }
+    }
 
-    private fun convertCharToMorse(c: Char) = when (c.toLowerCase()) {
-        'a' -> ".-"
-        'b' -> "-..."
-        'c' -> "-.-."
-        'd' -> "-.."
-        'e' -> "."
-        'f' -> "..-."
-        'g' -> "--."
-        'h' -> "...."
-        'i' -> ".."
-        'j' -> ".---"
-        'k' -> "-.-"
-        'l' -> ".-.."
-        'm' -> "--"
-        'n' -> "-."
-        'o' -> "---"
-        'p' -> ".--."
-        'q' -> "--.-"
-        'r' -> ".-."
-        's' -> "..."
-        't' -> "-"
-        'u' -> "..-"
-        'v' -> "...-"
-        'w' -> ".--"
-        'x' -> "-..-"
-        'y' -> "-.--"
-        'z' -> "--.."
-        '1' -> ".----"
-        '2' -> "..---"
-        '3' -> "...--"
-        '4' -> "....-"
-        '5' -> "....."
-        '6' -> "-...."
-        '7' -> "--..."
-        '8' -> "---.."
-        '9' -> "----."
-        '0' -> "-----"
-        else -> throw IllegalArgumentException("Not all characters are valid")
+    private fun textToSypher(s: String): String {
+        val morseArr = s.map { value -> convertCharToSypher(value) }
+        morseArr.joinToString(" ")
+
+        return morseArr.joinToString(" ")
+    }
+
+    private fun sypherToText(s:String): String{
+        val strs = s.split(" ").toTypedArray()
+        val letterArray= strs.map {value -> convertSypherToChar(value) }
+        letterArray.joinToString("")
+        return letterArray.toString()
+    }
+
+    private fun convertCharToSypher(c: Char) = when (c.toLowerCase()) {
+        'ა' -> " არვეზ"
+        'ბ' -> " ბაკმუნდ"
+        'გ' -> " გერვაზ"
+        'დ' -> " დაკმი"
+        'ე' -> " ეხმუტ"
+        'ვ' -> " ვარმა"
+        'ზ' -> " ზორო"
+        'თ' -> " თაკრო"
+        'ი' -> " ირმაზ"
+        'კ' -> " კოხრო"
+        'ლ' -> " ლამლი"
+        'მ' -> " მაშნი"
+        'ნ' -> "  ნიშხო"
+        'ო' -> "  ორლო"
+        'პ' -> "  პახმად"
+        'ჟ' -> " ჟემჟემ"
+        'რ' -> " როხროხ"
+        'ს' -> " სამპა"
+        'ტ' -> " ტიხრიხ"
+        'უ' -> " ულმოდ"
+        'ფ' -> " ფაშრუ"
+        'ქ' -> " ქაღლაშ"
+        'ღ' -> " ღიშმიშ"
+        'ყ' -> " ყალა"
+        'შ' -> " შიჭა"
+        'ც' -> " ცუპრუ"
+        'ძ' -> " ძაწო"
+        'წ' -> " წოღრა"
+        'ჭ' -> " ჭილა"
+        'ხ' -> " ხოხო"
+        'ჯ' -> " ჯაყრუ"
+        'ჰ' -> " ჰერი"
+        '1' -> " თერი"
+        '2' -> " რიო"
+        '3' -> " ისმა"
+        '4' -> " ხითო"
+        '5' -> " თიხუ"
+        '6' -> " ქვესი"
+        '7' -> " ვიშიდ"
+        '8' -> "არვ"
+        '9' -> " არცხ"
+        '0' -> " ილუნ"
+        ' ' -> " "
+
+        else -> Toast.makeText(requireContext(),"not all characters are valid", Toast.LENGTH_SHORT ).show()
+    }
+
+    private fun convertSypherToChar(c: String):Any = when (c.toLowerCase()){
+        "არვეზ" -> 'ა'
+        "ბაკმუნდ" ->  'ბ'
+        "გერვაზ" -> 'გ'
+        "დაკმი" ->'დ'
+        "ეხმუტ" -> 'ე'
+        "ვარმა" -> 'ვ'
+        "ზორო"->  'ზ'
+        "თაკრო"-> 'თ'
+        "ირმაზ"-> 'ი'
+        "კოხრო"-> 'კ'
+        "ლამლი"-> 'ლ'
+        "მაშნი"-> 'მ'
+        "ნიშხო" -> 'ნ'
+        "ორლო" ->'ო'
+        "პახმად"->  'პ'
+        "ჟემჟემ" -> 'ჟ'
+        "როხროხ"-> 'რ'
+        "სამპა" -> 'ს'
+        "ტიხრიხ" ->  'ტ'
+        "ულმოდ" -> 'უ'
+        "ფაშრუ" ->'ფ'
+        "ქაღლაშ"-> 'ქ'
+        "ღიშმიშ" -> 'ღ'
+        "ყალა"-> 'ყ'
+        "შიჭა" -> 'შ'
+        "ცუპრუ"-> 'ც'
+        "ძაწო"-> 'ძ'
+        "წოღრა"-> 'წ'
+        "ჭილა" -> 'ჭ'
+        "ხოხო" -> 'ხ'
+        "ჯაყრუ"->  'ჯ'
+        "ჰერი"-> 'ჰ'
+        "თერი" -> '1'
+        "რიო" -> '2'
+        "ისმა" -> '3'
+        "ხითო" -> '4'
+        "თიხუ" -> '5'
+        "ქვესი" ->'6'
+        "ვიშიდ" -> '7'
+        "არვ" -> '8'
+        "არცხ"->  '9'
+        "ილუნ" -> '0'
+
+        else -> Toast.makeText(requireContext(), "incorrect input", Toast.LENGTH_SHORT).show()
     }
 
 }
